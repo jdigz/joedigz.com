91612c05ad9a1a2ba1328ba8f47df6565bb3bea8
Joe DiGregorio <47089042+jdigz@users.noreply.github.com>
2021-04-02 19:14:11 -0400

    Created Footer component file
    Also added social links component file, added to App.js

---
+++ src/components/footer.js
@@ -0,0 +1,44 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import Button from './button';
+import SocialLinks from './socialLinks';
+
+const FooterWrapper = styled.div`
+  margin: 50px auto 100px auto;
+  max-width: 1025px;
+  text-align: center;
+`;
+const FooterHeader = styled.h1`
+  margin-bottom: 50px;
+  font-size: 72px;
+  font-weight: bold;
+  letter-spacing: -5%;
+  line-height: 100px;
+`;
+const FooterText = styled.div`
+  margin-bottom: 50px;
+  font-size: 30px;
+  
+`;
+
+class Footer extends PureComponent {
+  render() { 
+    return (
+      <FooterWrapper>
+        <FooterHeader>
+          You've come so far<br />
+          now it's time to make it official.
+        </FooterHeader>
+        <FooterText>
+          I'll make it easy for you, just click the button below<br/>
+          I would love to hear from you!
+        </FooterText>
+        <Button>
+          Send Email Now!
+        </Button>
+        <SocialLinks />
+      </FooterWrapper>
+      );
+  }
+}
+export default Footer;
---
+++ src/components/socialLinks.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+
+class SocialLinks extends PureComponent {
+  render() { 
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+ 
+export default SocialLinks;
